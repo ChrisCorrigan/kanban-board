40ae5cc81fad005998aab88cb086986d2ffe07e2
Chris Corrigan <chriscorrigan14@gmail.com>
2024-03-12 16:31:42 -0700

    add tailwind for css, starting to add main component

---
+++ src/components/boardList.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+import { getBoards } from "./firebaseUtils";
+
+const BoardList = () => {
+  const [boards, setBoards] = useState([]);
+
+  useEffect(() => {
+    const fetchBoards = async () => {
+      const boards = await getBoards();
+      setBoards(boards);
+    };
+
+    fetchBoards();
+  }, []);
+
+  return (
+    <div>
+      <h2>Boards</h2>
+      <ul>
+        {boards.map((board) => {
+          <li key={board.id}>{board.title}</li>;
+        })}
+      </ul>
+    </div>
+  );
+};
