3eb607dbf23797c246cfdd2b1ea6ba89b333120c
Chris Corrigan <chriscorrigan14@gmail.com>
2024-03-12 18:59:08 -0700

    adding react router and the navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HomePageView from "./views/HomePageView";
+import SignUpView from "./views/SignUpView";
+import LoginView from "./views/LoginView";
+import DashboardView from "./views/DashboardView";
+import BoardView from "./views/BoardView";
+
 function App() {
   return (
-    <div className="App">
-      {/* Your Kanban board component or other components here */}
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePageView />} />
+          <Route path="/signup" element={<SignUpView />} />
+          <Route path="/login" element={<LoginView />} />
+          <Route path="/dashboard" element={<DashboardView />} />
+          <Route path="/board/:id" element={<BoardView />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav className="bg-gray-800 p-4 text-white">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link to="/" className="text-xl font-bold">
+          Kanban App
+        </Link>
+        <div>
+          <Link to="/signup" className="mr-4 hover:text-gray-300">
+            Sign Up
+          </Link>
+          <Link to="/login" className="hover:text-gray-300">
+            Login
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
