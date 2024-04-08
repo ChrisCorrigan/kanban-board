c13f1e83d914af2aca3c1e04c62f9a7fddbdfcb2
Chris Corrigan <chriscorrigan14@gmail.com>
2024-04-07 20:25:28 -0700

    tailwind configuration

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
 import "./tailwind.css";
 import { BrowserRouter } from "react-router-dom";
