427fa0bec116c9da474b8b02720af3031c898399
Chris Corrigan <chriscorrigan14@gmail.com>
2024-04-09 19:22:51 -0700

    fix issue with logout from header not ending up on home page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,31 +7,33 @@ function Header() {
   const navigate = useNavigate();
   const { userLoggedIn } = useAuth();
 
+  async function handleLogout() {
+    await logOut();
+    navigate("/");
+  }
+
   return (
     <nav className="bg-gray-800 p-4 text-white">
       <div className="container">
         <nav className="flex">
           {userLoggedIn ? (
             <>
-              <Link to="/" className="p-4">
+              <Link to="/" className="p-4 hover:bg-gray-700 transition-colors duration-200">
                 Home
               </Link>
               <button
-                onClick={() => {
-                  logOut();
-                  navigate("/");
-                }}
-                className="p-4"
+                onClick={handleLogout}
+                className="p-4 hover:bg-gray-700 transition-colors duration-200"
               >
                 Logout
               </button>
             </>
           ) : (
             <>
-              <Link to="/login" className="py-2 px-4">
+              <Link to="/login" className="py-2 px-4 hover:bg-gray-700 transition-colors duration-200">
                 Login
               </Link>
-              <Link to="/signup" className="py-2 px-4">
+              <Link to="/signup" className="py-2 px-4 hover:bg-gray-700 transition-colors duration-200">
                 Register for a new account
               </Link>
             </>
