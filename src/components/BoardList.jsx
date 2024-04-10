cbf0583162bb23f0feacdf1073773f378600494f
Chris Corrigan <chriscorrigan14@gmail.com>
2024-04-09 21:44:39 -0700

    styling for the list of boards to make it more CTA obvious

--- src/components/BoardList.jsx
+++ src/components/BoardList.jsx
@@ -4,12 +4,13 @@ import { Link } from "react-router-dom";
 const BoardList = ({ boards }) => {
   return (
     <div>
-      <h2>Boards</h2>
       <ul>
         {boards.map((board) => {
           return (
             <li key={board.id}>
-              <Link to={`/board/${board.id}`}>{board.title}</Link>
+              <Link to={`/board/${board.id}`} className="hover:text-blue-500 transition duration-200">
+                &#9656; {board.title}
+              </Link>
             </li>
           );
         })}
