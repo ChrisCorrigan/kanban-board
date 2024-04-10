289f1b37275fc2850b3b1bf39a5cccc41b1a573f
Chris Corrigan <chriscorrigan14@gmail.com>
2024-04-10 10:59:26 -0700

    improve the look of the delete icon using an svg

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -77,8 +77,10 @@ const List = ({ list }) => {
               {card.title}
             </h4>
           )}
-          <button onClick={() => deleteCardHandler(card.id)} className="text-red-500 hover:text-red-700 ml-2">
-            x
+          <button onClick={() => deleteCardHandler(card.id)} className="ml-2">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-4 w-4 text-gray-500 hover:text-gray-700">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+            </svg>
           </button>
         </div>
       ))}
