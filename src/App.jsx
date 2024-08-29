9113c4b4bd515ebdd5c7fd68c589cbae0dd2e134
Chris Corrigan <chriscorrigan14@gmail.com>
2024-08-29 11:40:57 -0700

    prettyfying

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
-import { AuthProvider } from "./context/authContext";
-import { useRoutes } from "react-router-dom";
+import { AuthProvider } from './context/authContext'
+import { useRoutes } from 'react-router-dom'
 
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Dashboard from "./pages/Dashboard";
-import Home from "./pages/home";
-import SignUp from "./pages/Signup";
-import SignIn from "./pages/Signin";
-import Board from "./pages/Board";
-import CreateBoard from "./pages/CreateBoard";
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Dashboard from './pages/Dashboard'
+import Home from './pages/home'
+import SignUp from './pages/Signup'
+import SignIn from './pages/Signin'
+import Board from './pages/Board'
+import CreateBoard from './pages/CreateBoard'
 
 function App() {
   const routes = useRoutes([
-    { path: "/", element: <Home /> },
-    { path: "/signup", element: <SignUp /> },
-    { path: "/login", element: <SignIn /> },
-    { path: "/Home", element: <Home /> },
-    { path: "/dashboard", element: <Dashboard /> },
-    { path: "/board/:id", element: <Board /> },
-    { path: "/createboard", element: <CreateBoard /> },
-  ]);
+    { path: '/', element: <Home /> },
+    { path: '/signup', element: <SignUp /> },
+    { path: '/login', element: <SignIn /> },
+    { path: '/Home', element: <Home /> },
+    { path: '/dashboard', element: <Dashboard /> },
+    { path: '/board/:id', element: <Board /> },
+    { path: '/createboard', element: <CreateBoard /> },
+    { path: '/practice', element: <Practice /> },
+  ])
 
   return (
     <AuthProvider>
@@ -27,7 +28,7 @@ function App() {
       {routes}
       <Footer />
     </AuthProvider>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,57 +1,62 @@
-import React, { useEffect, useState } from "react";
-import { getCards, addCardToList, updateCard, deleteCard } from "../utils/firebaseUtils";
-import Card from "./Card";
+import React, { useEffect, useState } from 'react'
+import {
+  getCards,
+  addCardToList,
+  updateCard,
+  deleteCard,
+} from '../utils/firebaseUtils'
+import Card from './Card'
 
-import {DndContext} from '@dnd-kit/core';
-import {SortableContext} from '@dnd-kit/sortable';
-import {SortableItem} from './SortableItem';
+import { DndContext } from '@dnd-kit/core'
+import { SortableContext } from '@dnd-kit/sortable'
+import { SortableItem } from './SortableItem'
 
 const List = ({ list }) => {
-  const [cards, setCards] = useState([]);
-  const [editingCardId, setEditingCardId] = useState(null);
-  const [editingCardTitle, setEditingCardTitle] = useState("");
+  const [cards, setCards] = useState([])
+  const [editingCardId, setEditingCardId] = useState(null)
+  const [editingCardTitle, setEditingCardTitle] = useState('')
 
   useEffect(() => {
     getCards(list.id)
       .then((cardsData) => {
-        setCards(cardsData);
+        setCards(cardsData)
       })
       .catch((error) => {
-        console.log(error.message);
-      });
-  }, [list.id]);
+        console.log(error.message)
+      })
+  }, [list.id])
 
   const addCard = async () => {
-    const newCard = { title: "new card" };
+    const newCard = { title: 'new card' }
     try {
-      const cardRef = await addCardToList(list.id, newCard);
-      setCards((prevCards) => [...prevCards, { id: cardRef.id, ...newCard }]);
+      const cardRef = await addCardToList(list.id, newCard)
+      setCards((prevCards) => [...prevCards, { id: cardRef.id, ...newCard }])
     } catch (error) {
-      console.error("Error adding card: ", error);
+      console.error('Error adding card: ', error)
     }
-  };
+  }
 
   const saveCardTitle = async (id) => {
     try {
-      await updateCard(id, { title: editingCardTitle });
+      await updateCard(id, { title: editingCardTitle })
       setCards((prevCards) =>
         prevCards.map((card) =>
-          card.id === id ? { ...card, title: editingCardTitle } : card,
-        ),
-      );
-      setEditingCardId(null);
-      setEditingCardTitle("");
+          card.id === id ? { ...card, title: editingCardTitle } : card
+        )
+      )
+      setEditingCardId(null)
+      setEditingCardTitle('')
     } catch (error) {
-      console.error("Error updating card: ", error);
+      console.error('Error updating card: ', error)
     }
-  };
+  }
 
   const deleteCardHandler = async (id) => {
     try {
-      await deleteCard(id);
-      setCards((prevCards) => prevCards.filter((card) => card.id !== id));
+      await deleteCard(id)
+      setCards((prevCards) => prevCards.filter((card) => card.id !== id))
     } catch (error) {
-      console.error("Error deleting card: ", error);
+      console.error('Error deleting card: ', error)
     }
   }
 
@@ -59,8 +64,8 @@ const List = ({ list }) => {
     <div className="relative list bg-white shadow-md rounded px-4 py-6 mb-4 mr-4">
       <h3 className="font-bold text-xl mb-4">{list.title}</h3>
       {cards.map((card) => (
-        <Card 
-          key={card.id} 
+        <Card
+          key={card.id}
           card={card}
           editingCardId={editingCardId}
           editingCardTitle={editingCardTitle}
@@ -70,11 +75,14 @@ const List = ({ list }) => {
           deleteCardHandler={deleteCardHandler}
         />
       ))}
-      <button onClick={addCard} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mt-2 rounded focus:outline-none focus:shadow-outline">
+      <button
+        onClick={addCard}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mt-2 rounded focus:outline-none focus:shadow-outline"
+      >
         Add Card
       </button>
     </div>
-  );
-};
+  )
+}
 
-export default List;
+export default List
